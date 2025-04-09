056dc8b94abdbd884e35ee4ae29564c93bdd4c74
Trishathakur0221 <trishathakur0221@gmail.com>
2025-04-09 11:11:20 +0530

    hello

---
+++ subtraction.java
@@ -0,0 +1,10 @@
+public class subtraction {
+    public static void main(String[] args)
+    {
+       int a= 18;
+       int b= 15;
+       int c= a-b;
+       System.out.println(c);
+    }
+    
+}
