8aa376437ff0a169ddf2d9f28db53131fc420d4e
Trishathakur0221 <trishathakur0221@gmail.com>
2025-04-09 11:07:17 +0530

    hello

---
+++ basic.java
@@ -0,0 +1,10 @@
+public class basic
+ {
+    public static void main(String[] args)
+     {
+        System.out.println("hello"+10+10);
+
+        System.out.println( 10+10+"hello");
+     }
+    
+}
---
+++ may.java
@@ -0,0 +1,9 @@
+public class may 
+{
+    public static void main(String[] args) 
+ {
+    int a=8;
+    int b= 7;
+    System.out.println(a&b);
+ }
+}
