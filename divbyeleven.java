98f02d7142faf4c9f3e31011b35156634c2764aa
Trishathakur0221 <trishathakur0221@gmail.com>
2025-04-11 23:03:12 +0530

    hello

---
+++ divbyeleven.java
@@ -0,0 +1,10 @@
+public class divbyeleven {
+    public static void main(String [] args){
+        int num= 35;
+        if (num%11==0)
+        System.out.println("divisible by 11");
+        else
+        System.out.println("not divisible by 11");
+    }
+    
+}
---
+++ divbytwo.java
@@ -0,0 +1,10 @@
+public class divbytwo {
+    public static void main( String [] args){
+        int num = 9;
+        if (num%2==0)
+        System.out.println("divisible by 5");
+        else
+        System.out.println("not divisible by 5");
+    }
+    
+}
