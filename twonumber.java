24cf58e39396d350e2b85df15d8d3b02beb56cef
Trishathakur0221 <trishathakur0221@gmail.com>
2025-04-11 08:57:49 +0530

    hello

---
+++ twonumber.java
@@ -0,0 +1,10 @@
+public class twonumber {
+    public static void main(String [] args){
+        int a=9, b=75;
+        if (a>b)
+        System.out.println("a is greater");
+        else 
+        System.out.println("b is greater");
+    }
+    
+}
