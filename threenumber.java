e9e410a16c8932c9577819be2e558916988abd4f
Trishathakur0221 <trishathakur0221@gmail.com>
2025-04-11 09:02:38 +0530

    hello

---
+++ threenumber.java
@@ -0,0 +1,16 @@
+public class threenumber {
+    public static void main(String [] args){
+       int  a = 5, b=6 , c=7;
+       if(a>b) 
+       System.out.println("a is greater");
+
+       if (a>c)
+       System.out.println("a is greater");
+       else if (b>c)
+       System.out.println("b is greater");
+       else 
+       System.out.println("c is greater");
+
+    }
+    
+}
