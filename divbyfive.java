0f886a2565ffaff38935c086789bd7dde07fc5b2
Trishathakur0221 <trishathakur0221@gmail.com>
2025-04-11 22:01:56 +0530

    hello

---
+++ divbyfive.java
@@ -0,0 +1,12 @@
+public class divbyfive {
+    public static void main(String [] args){
+        int num= 49;
+        if(num%5==0)
+            System.out.println("divisible by 5");
+            else
+            System.out.println("not divisible by 5");
+        
+
+    }
+    
+}
