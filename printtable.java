c4402a8965ac209ef39b0b8fa6ca2c4cd7f19586
Trishathakur0221 <trishathakur0221@gmail.com>
2025-04-12 17:58:34 +0530

    hello

--- printtable.java
+++ printtable.java
@@ -1,11 +1,10 @@
 public class printtable {
     public static void main(String [] args){
-        int n=6;
+        int n=97;
       
         for (int i=1;i<=10;i++){
-         n*i;
+            System.out.println(n*i);
         }
-        System.out.println(n*i);
     }
 
        
