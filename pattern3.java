77c276b5b48ce505de355fffdccd1a2a6c3e152b
Trishathakur0221 <trishathakur0221@gmail.com>
2025-04-15 14:00:22 +0530

    hello

---
+++ pattern3.java
@@ -0,0 +1,15 @@
+public class pattern3
+ {
+    public static void main(String [] args)
+    {
+        for(int i=1;i<=5;i++){
+     for (int j=5;j>=i;j--){
+            System.out.println(" ");
+        }
+        }
+        for(int k=5;k>=1;k--){
+        System.out.println("*");
+    }
+    
+    }
+ }
