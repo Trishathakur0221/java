75ed2decae17b1019c317277456bdafc3731fdeb
Trishathakur0221 <trishathakur0221@gmail.com>
2025-11-03 07:40:42 +0530

    hello

---
+++ sumusingdouble.java
@@ -0,0 +1,12 @@
+public class sumusingdouble {
+    public static void main(String[] args){
+    double x1= 90;
+    double x2= 91;
+    double x3=92;
+    double x4=93;
+    double x5=94;
+    double sum = x1 + x2 + x3 + x4 + x5;
+    System.out.println("The sum is: " + sum);
+
+    }
+}
