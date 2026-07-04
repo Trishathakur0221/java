35f42d510a686256aee142031af9a9e23750623d
Trishathakur0221 <trishathakur0221@gmail.com>
2026-07-04 10:15:48 +0530

    hii

--- Hashmap.java
+++ Hashmap.java
@@ -12,21 +12,25 @@ public class Hashmap {
 
         System.out.println("Map: " + map);
 
-        // // searching for an element
-        // if(map.containsKey("UA")) {
-        //     System.out.println("UA is present in the map.");
-        // } else {
-        //     System.out.println("UA is not present in the map.");
-        // }
+        // searching for an element
+        if(map.containsKey("UA")) {
+            System.out.println("UA is present in the map.");
+        } else {
+            System.out.println("UA is not present in the map.");
+        }
 
-        // //get the value associated with a key
-        // System.out.println("Value associated with 'US': " + map.get("US"));        
-        // System.out.println("Value associated with 'India': " + map.get("India"));
+        //get the value associated with a key
+        System.out.println("Value associated with 'US': " + map.get("US"));        
+        System.out.println("Value associated with 'India': " + map.get("India"));
 
         // //iterator to traverse the map
         for(Map.Entry <String , Integer> e:map.entrySet()){
             System.out.println(e.getKey());
             System.out.println(e.getValue());
         }
+
+        //removing an element
+        map.remove("USA"); // Remove the entry with key "USA"
+        System.out.println("Map after removing USA: " + map);
     }
 }
